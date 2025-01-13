49b7b0966256a117a3e5cdb26d271760d4c43ea3
Abdulhefiz Abdurahman <105729069+Abdl-Hafeez@users.noreply.github.com>
2025-01-13 15:08:36 +0300

    Add keyboard support

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function operator(num1, operator, num2) {
     if(operator === '*')  {
         return num1 * num2;
     }
-    if(operator === '÷')  {
+    if(operator === '/')  {
         return num1 / num2;
     }
     if(operator === '%')  {
@@ -106,4 +106,62 @@ backspaceBtn.addEventListener('click', ()  => {
         }
     }
     
+})
+
+document.addEventListener('keydown', (event) => {
+    // let keyboardOperator = '+' || '-' || '*' || '/' || '%';
+    if(!isNaN(event.key) || event.key === '.') {
+        console.log(typeof (event.key));
+        displayInput(event.key); 
+        if(!operatorSign) {
+            firstNumber = display.textContent;
+            currentDisplay = firstNumber;   
+        } else {
+            secondNumber = secondNumber ? secondNumber + event.key : event.key;
+            currentDisplay = firstNumber + operatorSign + secondNumber;
+        }   
+
+        if(firstNumber.includes('.') && !operatorSign || operatorSign && !secondNumber || operatorSign && secondNumber.includes('.')) {
+            dotBtn.disabled = true;
+        }else {
+            dotBtn.disabled = false;
+        } 
+    } else if((event.key === '+') || (event.key === '-') || (event.key === '*') || (event.key === '/') || (event.key === '%')) {
+        console.log(event.key + 'key pressed');
+        if(firstNumber) {
+            displayInput(event.key);
+            operatorSign = event.key;
+            currentDisplay = firstNumber + operatorSign;
+            display.textContent = currentDisplay;
+        } 
+    } else if (event.key === '=' || event.key === 'Enter') {
+        console.log(event.key + 'key pressed');
+        const result = operator(Number(firstNumber), operatorSign, Number(secondNumber));
+        let roundedResult;
+        if(firstNumber && operatorSign && secondNumber) {
+        if (result.toString().includes('.') && result.toString().length > 10) {
+            roundedResult = Number(result.toFixed(10));
+        } else {
+            roundedResult = result; 
+        }
+        display.textContent = roundedResult;
+        firstNumber = roundedResult.toString();
+        operatorSign = null;
+        secondNumber = null;
+        currentDisplay = firstNumber;
+        dotBtn.disabled = false;
+    }
+    } else if (event.key === 'Backspace' || event.key === 'Delete') {
+        console.log(event.key + 'key pressed');
+        if(currentDisplay.length > 0) {
+            const newDisplay = currentDisplay.slice(0, -1);
+            display.textContent = newDisplay;
+            currentDisplay = newDisplay;
+            if(secondNumber && currentDisplay.includes(operatorSign)) {
+                secondNumber = secondNumber.slice(0, -1);
+            } else  {
+                firstNumber = firstNumber.slice(0, -1);
+            }
+        }
+    }
 })
